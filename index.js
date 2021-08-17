97452d18ab7cc77ac03ad07eb191cd581dfd5f0d
nikiljos <57913645+nikiljos@users.noreply.github.com>
2021-08-17 19:10:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,118 @@
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+const express = require('express')
+const app = express()
+
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const tips = require('./tips.js')
+
+app.get('*', (req, res) => {
+    res.status(404).send("<h1>Sorry, Nothing's here</h1>")
+})
+
+app.post('/', function(req, res) {
+    // console.log(req.body.Body)
+
+    if (req.body.Body != undefined) {
+        sendResponse(req.body);
+        res.status(200).send("success");
+    } else {
+        res.status(400).send("failure")
+    }
+
+
+
+})
+
+async function sendResponse(data) {
+
+    let input = data.Body;
+
+    components = input.split(" ");
+    // console.log(components)
+    let param = ""
+
+    for (i = 1; i < components.length; i++) {
+        param += ` ${components[i]}`
+    }
+    if (param[0] == " ") {
+        param = param.substring(1)
+    }
+
+
+    let paramEncode = encodeURI(param)
+    command = components[0].toLowerCase()
+
+    param = param.toUpperCase();
+
+    // console.log(command, param)
+
+    if (command == "doc" && param) {
+        client.messages
+            .create({
+                from: `whatsapp:${process.env.FROM_NUMBER}`,
+                body: `Click this link to search for Dr. ${param} on *QKdoc*\n\nhttps://www.qkdoc.com/create-appointment/?search=${paramEncode}`,
+                to: data.From
+            })
+            .then(message => console.log(data.From, "doc message", message.sid));
+
+    } else if (command == "buy" && param) {
+        client.messages
+            .create({
+                from: `whatsapp:${process.env.FROM_NUMBER}`,
+                body: `Click these links to buy ${param} on the given sites\n\n*Netmeds*\nhttps://www.netmeds.com/catalogsearch/result?q=${paramEncode}\n\n*1mg*\nhttps://www.1mg.com/search/all?name=${paramEncode}\n\n*Pharmeasy*\nhttps://pharmeasy.in/search/all?name=${paramEncode}`,
+                to: data.From
+            })
+            .then(message => console.log(data.From, "buy message", message.sid));
+
+    } else if (command == "consult") {
+        client.messages
+            .create({
+                from: `whatsapp:${process.env.FROM_NUMBER}`,
+                body: `Here are some popular Online Consultation services\n\n*Tata Health*\nhttps://www.tatahealth.com/insta-doc\n\n*Appolo 247*\nhttps://www.apollo247.com/\n\n*1mg*\nhttps://www.1mg.com/online-doctor-consultation\n\n*Practo*\nhttps://www.practo.com/consult`,
+                to: data.From
+            })
+            .then(message => console.log(data.From, "buy message", message.sid));
+
+    } else if (command == "health" || command == "tip" || command == "tips") {
+
+        let n = Math.floor(Math.random() * tips.length)
+        client.messages
+            .create({
+                from: `whatsapp:${process.env.FROM_NUMBER}`,
+                mediaUrl: [tips[n].url],
+                body: `_Health Tip_\n\n*${tips[n].head}*\n\n${tips[n].disc}\n\n_Source:_ WHO`,
+                to: data.From
+            })
+            .then(message => console.log(data.From, "tip message", message.sid));
+
+    } else if (command == "help") {
+        client.messages
+            .create({
+                from: `whatsapp:${process.env.FROM_NUMBER}`,
+                body: `Send any of these commands and see the magic🗝️\n\n*doc* _DOCTOR'S NAME_ 👉 to search for a doctor\n\n*buy* _MEDICINE NAME_ 👉 to search and buy a medicine\n\n*consult online* 👉 Links to popular online doctor consultation services\n\n*health tips* 👉 to get a health tip\n\n`,
+                to: data.From
+            })
+            .then(message => console.log(data.From, "help message", message.sid));
+
+    } else {
+        client.messages
+            .create({
+                from: `whatsapp:${process.env.FROM_NUMBER}`,
+                body: `Seems like you entered an invalid command😯\n\nSend *help* to list out all available commands🤠`,
+                to: data.From
+            })
+            .then(message => console.log(data.From, "error message", message.sid));
+    }
+
+
+}
+
+app.listen(process.env.PORT || 5000, () => console.log("server running"))
---
+++ tips.js
@@ -0,0 +1,96 @@
+module.exports = [{
+        "head": "Eat a healthy diet",
+        "disc": "Eat a combination of different foods, including fruit, vegetables, legumes, nuts and whole grains. Adults should eat at least five portions (400g) of fruit and vegetables per day. You can improve your intake of fruits and vegetables by always including veggies in your meal; eating fresh fruit and vegetables as snacks; eating a variety of fruits and vegetables; and eating them in season. By eating healthy, you will reduce your risk of malnutrition and noncommunicable diseases (NCDs) such as diabetes, heart disease, stroke and cancer.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/1-20190529-091438-lr.tmb-1366v.jpg?sfvrsn=9ba890c9_2"
+
+
+    },
+    {
+        "head": "Consume less salt and sugar",
+        "disc": "Filipinos consume twice the recommended amount of sodium, putting them at risk of high blood pressure, which in turn increases the risk of heart disease and stroke. Most people get their sodium through salt. Reduce your salt intake to 5g per day, equivalent to about one teaspoon. It’s easier to do this by limiting the amount of salt, soy sauce, fish sauce and other high-sodium condiments when preparing meals; removing salt, seasonings and condiments from your meal table; avoiding salty snacks; and choosing low-sodium products. On the other hand, consuming excessive amounts of sugars increases the risk of tooth decay and unhealthy weight gain. In both adults and children, the intake of free sugars should be reduced to less than 10% of total energy intake. This is equivalent to 50g or about 12 teaspoons for an adult. WHO recommends consuming less than 5% of total energy intake for additional health benefits. You can reduce your sugar intake by limiting the consumption of sugary snacks, candies and sugar-sweetened beverages.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/2-who-056764-orig.tmb-1366v.jpg?sfvrsn=c20a162e_2"
+
+    },
+    {
+        "head": "Reduce intake of harmful fats",
+        "disc": "Fats consumed should be less than 30% of your total energy intake. This will help prevent unhealthy weight gain and NCDs. There are different types of fats, but unsaturated fats are preferable over saturated fats and trans-fats. WHO recommends reducing saturated fats to less than 10% of total energy intake; reducing trans-fats to less than 1% of total energy intake; and replacing both saturated fats and trans-fats to unsaturated fats. The preferable unsaturated fats are found in fish, avocado and nuts, and in sunflower, soybean, canola and olive oils; saturated fats are found in fatty meat, butter, palm and coconut oil, cream, cheese, ghee and lard; and trans-fats are found in baked and fried foods, and pre-packaged snacks and foods, such as frozen pizza, cookies, biscuits, and cooking oils and spreads.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/3-who-056149-img.tmb-1366v.jpg?sfvrsn=c0dc2291_2"
+    },
+    {
+        "head": "Avoid harmful use of alcohol",
+        "disc": "There is no safe level for drinking alcohol. Consuming alcohol can lead to health problems such as mental and behavioural disorders, including alcohol dependence, major NCDs such as liver cirrhosis, some cancers and heart diseases, as well as injuries resulting from violence and road clashes and collisions.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/4-who-056030-img.tmb-1366v.jpg?sfvrsn=227d74e_2"
+    },
+    {
+        "head": "Don’t smoke",
+        "disc": "Smoking tobacco causes NCDs such as lung disease, heart disease and stroke. Tobacco kills not only the direct smokers but even non-smokers through second-hand exposure. Currently, there are around 15.9 million Filipino adults who smoke tobacco but 7 in 10 smokers are interested or plan to quit. If you are currently a smoker, it’s not too late to quit. Once you do, you will experience immediate and long-term health benefits. If you are not a smoker, that’s great! Do not start smoking and fight for your right to breathe tobacco-smoke-free air.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/5-f9-05052016-ph-03850-lr.tmb-1366v.jpg?sfvrsn=835b05bf_2"
+    },
+    {
+        "head": "Be active",
+        "disc": "Physical activity is defined as any bodily movement produced by skeletal muscles that requires energy expenditure. This includes exercise and activities undertaken while working, playing, carrying out household chores, travelling, and engaging in recreational pursuits. The amount of physical activity you need depends on your age group but adults aged 18-64 years should do at least 150 minutes of moderate-intensity physical activity throughout the week. Increase moderate-intensity physical activity to 300 minutes per week for additional health benefits.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/6-f2-300032016-ph-06573-lr.tmb-1366v.jpg?sfvrsn=f4bc39b4_2"
+
+    },
+    {
+        "head": "Check your blood pressure regularly",
+        "disc": "Hypertension, or high blood pressure, is called a “silent killer”. This is because many people who have hypertension may not be aware of the problem as it may not have any symptoms. If left uncontrolled, hypertension can lead to heart, brain, kidney and other diseases. Have your blood pressure checked regularly by a health worker so you know your numbers. If your blood pressure is high, get the advice of a health worker. This is vital in the prevention and control of hypertension.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/7-img-3982-lr.tmb-1366v.jpg?sfvrsn=20ea404c_2"
+    },
+    {
+        "head": "Get tested",
+        "disc": "Getting yourself tested is an important step in knowing your health status, especially when it comes to HIV, hepatitis B, sexually-transmitted infections (STIs) and tuberculosis (TB). Left untreated, these diseases can lead to serious complications and even death. Knowing your status means you will know how to either continue preventing these diseases or, if you find out that you’re positive, get the care and treatment that you need. Go to a public or private health facility, wherever you are comfortable, to have yourself tested.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/8-img-6015-lr.tmb-1366v.jpg?sfvrsn=be30b00c_2"
+    },
+    {
+        "head": "Get vaccinated",
+        "disc": "Vaccination is one of the most effective ways to prevent diseases. Vaccines work with your body’s natural defences to build protection against diseases like cervical cancer, cholera, diphtheria, hepatitis B, influenza, measles, mumps, pneumonia, polio, rabies, rubella, tetanus, typhoid, and yellow fever. In the Philippines, free vaccines are provided to children 1 year old and below as part of the Department of Health’s routine immunization programme. If you are an adolescent or adult, you may ask your physician if to check your immunization status or if you want to have yourself vaccinated.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/9-img-1210-lr.tmb-1366v.jpg?sfvrsn=10567f2d_2"
+    },
+    {
+        "head": "Cover your mouth when coughing or sneezing",
+        "disc": "Diseases such as influenza, pneumonia and tuberculosis are transmitted through the air. When an infected person coughs or sneezes, infectious agents may be passed on to others through airborne droplets. When you feel a cough or sneeze coming on, make sure you have covered your mouth with a face mask or use a tissue then dispose it carefully. If you do not have a tissue close by when you cough or sneeze, cover your mouth as much as possible with the crook (or the inside) of your elbow.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/11-who-012542-orig-crop.tmb-1366v.jpg?sfvrsn=fbda58f9_2"
+    },
+    {
+        "head": "Prevent mosquito bites",
+        "disc": "Mosquitoes are one of the deadliest animals in the world. Diseases like dengue, chikungunya, malaria and lymphatic filariasis are transmitted by mosquitoes and continue to affect Filipinos. You can take simple measures to protect yourself and your loved ones against mosquito-borne diseases. If you’re traveling to an area with known mosquito-borne diseases, consult a physician for a vaccine to prevent diseases such as Japanese encephalitis and yellow fever or if you need to take antimalarial medicines. Wear light-coloured, long-sleeved shirts and pants and use insect repellent. At home, use window and door screens, use bed nets and clean your surroundings weekly to destroy mosquito breeding sites.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/12-f7-28102015-ph-01283-lr.tmb-1366v.jpg?sfvrsn=a9ed915f_2"
+    },
+    {
+        "head": "Follow traffic laws",
+        "disc": "Road crashes claim over one million lives around the world and millions more are injured. Road traffic injuries are preventable through a variety of measures implemented by the government such as strong legislation and enforcement, safer infrastructure and vehicle standards, and improved post-crash care. You yourself can also prevent road crashes by ensuring that you follow traffic laws such as using the seatbelt for adults and child restraint for your kids, wearing a helmet when riding a motorcycle or bicycle, not drinking and driving, and not using your mobile phone while driving.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/13-who-059700-orig-lr.tmb-1366v.jpg?sfvrsn=19066fbd_2"
+    },
+    {
+        "head": "Drink only safe water",
+        "disc": "Drinking unsafe water can lead to water-borne diseases such as cholera, diarrhoea, hepatitis A, typhoid and polio. Globally, at least 2 billion people use a drinking water source contaminated with faeces. Check with your water concessionaire and water refilling station to ensure that the water you’re drinking is safe. In a setting where you are unsure of your water source, boil your water for at least one minute. This will destroy harmful organisms in the water. Let it cool naturally before drinking",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/14-mg-9033-lr.tmb-1366v.jpg?sfvrsn=eca64271_2"
+    },
+    {
+        "head": "Talk to someone you trust if you're feeling down",
+        "disc": "Depression is a common illness worldwide with over 260 million people affected. Depression can manifest in different ways, but it might make you feel hopeless or worthless, or you might think about negative and disturbing thoughts a lot or have an overwhelming sense of pain. If you’re going through this, remember that you are not alone. Talk to someone you trust such as a family member, friend, colleague or mental health professional about how you feel. If you feel that you are in danger of harming yourself, contact the National Center for Mental Health hotline at 0917-899-USAP (8727).",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/16-mg-8143-lr.tmb-1366v.jpg?sfvrsn=1d58571b_2"
+    },
+    {
+        "head": "Take antibiotics only as prescribed",
+        "disc": "Antibiotic resistance is one of the biggest public health threats in our generation. When antibiotics lose their power, bacterial infections become harder to treat, leading to higher medical costs, prolonged hospital stays, and increased mortality. Antibiotics are losing their power because of misuse and overuse in humans and animals. Make sure you only take antibiotics if prescribed by a qualified health professional. And once prescribed, complete the treatment days as instructed. Never share antibiotics",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/17-img-9606-lr.tmb-1366v.jpg?sfvrsn=7f694005_2"
+    },
+
+    {
+        "head": "Clean your hands properly",
+        "disc": "Hand hygiene is critical not only for health workers but for everyone. Clean hands can prevent the spread of infectious illnesses. You should handwash using soap and water when your hands are visibly soiled or handrub using an alcohol-based product.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/18-img-7843-lr.tmb-1366v.jpg?sfvrsn=a250f251_2"
+    },
+    {
+        "head": "Prepare your food correctly",
+        "disc": "Unsafe food containing harmful bacteria, viruses, parasites or chemical substances, causes more than 200 diseases – ranging from diarrhoea to cancers. When buying food at the market or store, check the labels or the actual produce to ensure it is safe to eat. If you are preparing food, make sure you follow the Five Keys to Safer Food: (1) keep clean; (2) separate raw and cooked; (3) cook thoroughly; (4) keep food at safe temperatures; and (5) use safe water and raw materials.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/19-vlcsnap-2019-06-07-10h12m40s630.tmb-1366v.jpg?sfvrsn=1b5752fa_2"
+    },
+    {
+        "head": "Have regular check-ups",
+        "disc": "Regular check-ups can help find health problems before they start. Health professionals can help find and diagnose health issues early, when your chances for treatment and cure are better. Go to your nearest health facility to check out the the health services, screenings and treatment that are accessible to you.",
+        "url": "https://www.who.int/images/default-source/wpro/countries/philippines/feature-stories/20-f36-30032017-ph-4345-lr.tmb-1366v.jpg?sfvrsn=8a631e47_2"
+    }
+]
